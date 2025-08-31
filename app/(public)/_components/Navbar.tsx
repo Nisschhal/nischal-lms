3271d8fcb3c0b5cccb0cba674050807e73e9dc44
Nischal Puri <mrnischalpuri@gmail.com>
2025-08-31 22:42:38 +0545

    Signout and userdropdown created

--- app/(public)/_components/Navbar.tsx
+++ app/(public)/_components/Navbar.tsx
@@ -1,13 +1,29 @@
+"use client"
+import { buttonVariants } from "@/components/ui/button"
+import { ThemeToggle } from "@/components/ui/themeToggle"
+import { authClient } from "@/lib/auth-client"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
+import ProfileDropdown from "./Profile"
+
+const navigationItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  { name: "Courses", href: "/courses" },
+  { name: "Dashboard", href: "/dashboard" },
+]
 
 const Navbar = () => {
+  const { data: session, isPending } = authClient.useSession()
+
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
       {/* logo */}
-      <div className="container flex min-h-16 items-center mx-auto">
-        <Link href="/" className="flex items-center gap-2">
+      <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+        <Link href="/" className="flex items-center gap-2 mr-4">
           <Image
             src="/logo.svg"
             className="bg-[#a3570f] rounded-md p-1"
@@ -17,6 +33,45 @@ const Navbar = () => {
           />
           <span className="font-bold"> NischalLMS.</span>
         </Link>
+
+        {/* Desktop Nav Menu */}
+        <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <div className="flex items-center space-x-2 md:space-x-6">
+            {navigationItems.map((item, index) => (
+              <Link
+                href={item.href}
+                key={index}
+                className="text-sm font-medium text-white/80 transition-colors hover:text-primary"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <ThemeToggle />
+
+            {isPending ? null : session ? (
+              <ProfileDropdown
+                email={session.user.email}
+                name={session.user.name}
+                image={session.user.image || ""}
+              />
+            ) : (
+              <>
+                <Link
+                  href={"/login"}
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Login
+                </Link>
+                <Link href={"/login"} className={buttonVariants()}>
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
       </div>
     </header>
   )
---
+++ app/(public)/_components/Profile.tsx
@@ -0,0 +1,115 @@
+import {
+  BoltIcon,
+  BookOpenIcon,
+  ChevronDownIcon,
+  Home,
+  Layers2Icon,
+  LayoutDashboard,
+  LogOutIcon,
+  PinIcon,
+  UserPenIcon,
+} from "lucide-react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
+import { authClient } from "@/lib/auth-client"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+
+interface ProfileDropdownProps {
+  email: string
+  name: string
+  image: string
+}
+export default function ProfileDropdown({
+  email,
+  name,
+  image,
+}: ProfileDropdownProps) {
+  const router = useRouter()
+  const handleSignOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/")
+          toast.success("Signed out Successfully!")
+        },
+        onError: (error) => {
+          toast.error(error.error.message || "Error signing out")
+        },
+      },
+    })
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+          <Avatar>
+            <AvatarImage src={image} alt="Profile image" />
+            <AvatarFallback>{name.charAt(0).toUpperCase()}</AvatarFallback>
+          </Avatar>
+          <ChevronDownIcon
+            size={16}
+            className="opacity-60"
+            aria-hidden="true"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="max-w-64">
+        <DropdownMenuLabel className="flex min-w-0 flex-col">
+          <span className="text-foreground truncate text-sm font-medium">
+            {name}
+          </span>
+          <span className="text-muted-foreground truncate text-xs font-normal">
+            {email}
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/">
+              <Home size={16} className="opacity-60" aria-hidden="true" />
+              <span>Home</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/courses">
+              <BookOpenIcon
+                size={16}
+                className="opacity-60"
+                aria-hidden="true"
+              />
+              <span>Courses</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/dashboard">
+              <LayoutDashboard
+                size={16}
+                className="opacity-60"
+                aria-hidden="true"
+              />
+              <span>Dashboard</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleSignOut}>
+          <LogOutIcon size={16} className="opacity-60" aria-hidden="true" />
+          <span>Logout</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
